bee6bbf7f0c5d285f893f4795f27d970113190a5
Robert Clarke <r.jason.clarke@gmail.com>
2020-09-15 01:23:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution here!
 append = ["Milo", "Otis", "Garfield"]
-append.pop("Odie")
+append.pop("Odie");
 prepend = ["Milo", "Otis", "Garfield"]
 removeLast = ["Milo", "Otis", "Garfield"]
 removeFirst = ["Milo", "Otis", "Garfield"]
