c81d40c1e85f74476a187b455f4789271822aeda
Robert Clarke <r.jason.clarke@gmail.com>
2020-09-15 01:30:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const append = ["Milo", "Otis", "Garfield"];
 append.push("Odie");
 const prepend = ["Milo", "Otis", "Garfield"];
-prepend.unshift("Odie")
+prepend.unshift("Odie");
 const removeLast = ["Milo", "Otis", "Garfield"];
+removeLast.pop("Garfield");
 const removeFirst = ["Milo", "Otis", "Garfield"];
